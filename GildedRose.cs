9dea4f31ecc27105d91dbaeca772d372f3a5a5d0
MortaPlyciute <morta.plyciuraityte@gmail.com>
2019-11-17 18:43:15 +0200

    Refactoring GildedRose.cs
    Fully rafactoring. Special Items promblem is not solved yet. Everything else more or less.

--- GildedRose.cs
+++ GildedRose.cs
@@ -5,85 +5,89 @@ namespace csharp
     public class GildedRose
     {
         IList<Item> Items;
-        public GildedRose(IList<Item> Items)
+
+		List<string> specialItemsQualityRuleMax = new List<string>() { "Sulfuras" };
+		List<string> specialItemsSellIn = new List<string>() { "Sulfuras" };
+		List<string> specialItemsQualityRulePassSellInDrop = new List<string>() { "Backstage passes" };
+
+		int maxItemQuality = 50;
+
+		public GildedRose(IList<Item> Items)
         {
             this.Items = Items;
         }
 
         public void UpdateQuality()
         {
-            for (var i = 0; i < Items.Count; i++)
-            {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (Items[i].Quality > 0)
-                    {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
-                    }
-                }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].SellIn < 11)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    Items[i].SellIn = Items[i].SellIn - 1;
-                }
-
-                if (Items[i].SellIn < 0)
-                {
-                    if (Items[i].Name != "Aged Brie")
-                    {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].Quality > 0)
-                            {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-                        }
-                    }
-                }
-            }
+			foreach (Item item in Items)
+			{
+				item.Quality = CalculateNewQualityValue(item.Name, item.Quality);
+				item.SellIn = GetQualityChangeOfValue(item.Name, item.SellIn);
+			}
         }
-    }
+
+		public int GetQualityChangeOfValue(string itemName, int itemSellIn)
+		{
+			if (itemName.Contains("Aged Brie"))
+				return 1;
+			if (itemName.Contains("Sulfuras"))
+				return 0;
+			if (itemName.Contains("Conjured") && itemSellIn > 0)
+				return -2;
+			else if (itemName.Contains("Conjured"))
+				return -4;
+			if (itemName.Contains("Backstage passes") && itemSellIn > 10)
+				return 1;
+			else if (itemName.Contains("Backstage passes") && itemSellIn > 5)
+				return 2;
+			else if (itemName.Contains("Backstage passes"))
+				return 3;
+
+			if (itemSellIn > 0)
+				return -1;
+
+			return -2;
+		}
+
+		public int CalculateNewQualityValue(string itemName, int itemQuality, int itemSellIn)
+		{
+			int qualityChange = GetQualityChangeOfValue(item.Name);
+
+			if (IsItSpecialItemQualityRulePassSellInDrop(itemName) && itemSellIn <= 0)
+				return 0;
+
+			itemQuality = itemQuality + qualityChange;
+
+			if (itemQuality > maxItemQuality && !IsItSpecialItemQualityRuleMax(itemName))
+				itemQuality = maxItemQuality;
+
+			if (itemQuality < 0)
+				itemQuality = 0;
+
+			return itemQuality;
+		}
+
+		public int CalculateNewSellIn(string itemName, int itemSellIn)
+		{
+			if (IsItSpecialItemSellIn(itemName))
+				return itemSellIn;
+
+			return itemSellIn--;
+		}
+
+		public bool IsItSpecialItemQualityRuleMax(string itemName)
+		{
+
+		}
+
+		public bool IsItSpecialItemSellIn(string itemName)
+		{
+
+		}
+
+		public bool IsItSpecialItemQualityRulePassSellInDrop(string itemName)
+		{
+
+		}
+	}
 }
